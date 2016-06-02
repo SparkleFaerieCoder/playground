c894043072f7ba7b2f35d5344201a59eb09a9085
Alexandra Moss <amoss@catalystsolves.com>
2016-06-02 13:01:27 -0700

    finished tutorial for FunFactsActivity app.

--- FunFacts_s5v2/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ FunFacts_s5v2/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -9,9 +10,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class FunFactsActivity extends AppCompatActivity {
+   // Declare our classes and tags
     public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
+
     // Declare our View variables
     private TextView mFactTextView;
     private Button mShowFactButton;
@@ -42,6 +45,6 @@ public class FunFactsActivity extends AppCompatActivity {
         mShowFactButton.setOnClickListener(listener);
 
         Toast.makeText(FunFactsActivity.this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
-
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
